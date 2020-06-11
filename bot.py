58310f157055a9845320c70886a11637b985cb0b
Mike <None>
2020-06-11 17:51:38 -0600

    Added ability to pin a given string to the current channel

--- bot.py
+++ bot.py
@@ -21,6 +21,8 @@ PREFIX = '.bosco '
 client = discord.Client()
 bot = commands.Bot(command_prefix=PREFIX)
 
+#TODO: Help command
+
 @bot.event
 async def on_ready():
     print("Rock and Stone Miners! (Bosco is ready)")
@@ -61,15 +63,29 @@ async def on_raw_message_delete(rawevent):
                 await audit_channel.send("An exception occured while trying to retrieve message <" + str(rawevent.message_id) + "> from channel __#" + bot.get_channel(rawevent.channel_id).name + "__.")
             
         else:
-            await audit_channel.send("Deleted message by *" + rawevent.cached_message.author.name +"* at " +rawevent.cached_message.created_at.strftime("%m/%d/%Y, %H:%M:%S") + " in channel __#" + rawevent.cached_message.channel.name + "__ :\n " + rawevent.cached_message.content)
+            #TODO: Use bot.prefix()
+            if rawevent.cached_message.content[0:6] == '.bosco':
+                print("deleted message is a bot command, skipping...")
+                return None
+            else:
+                await audit_channel.send("Deleted message by *" + rawevent.cached_message.author.name +"* at " +rawevent.cached_message.created_at.strftime("%m/%d/%Y, %H:%M:%S") + " in channel __#" + rawevent.cached_message.channel.name + "__ :\n " + rawevent.cached_message.content)
 
 @bot.event
 async def on_message_edit(before,after):
+    print("on_message_edit was called")
     if before.channel.name == 'bosco-audit-log':
         print("edit in #bosco-audit-log , skipping...")
         return None
     audit_channel = await lookup_channel_server(before.guild,'bosco-audit-log')
     if after.content[0:4] == 'http':
+        print("message is a link, skipping...")
+        return None
+    #TODO: use bot.prefix()
+    elif after.content[0:6] == '.bosco':
+        print("message is a prefix, skipping...")
+        return None
+    elif after.pinned:
+        print("message is pinned, skipping...")
         return None
     else:
         await audit_channel.send("Message edited by *" + after.author.name + "* in __#" + after.channel.name + "__ :\n **BEFORE** : " + before.content + "\n** AFTER** : " + after.content)  
@@ -114,6 +130,16 @@ async def teamkills(ctx, member: discord.Member, command: str):
   #Remove bot command from the channel.
   await ctx.message.delete()
   
+
+#TODO: Override for pin by message id
+### Pin message to channel command was invoked in
+### REQUIRES: manage_roles
+@bot.command()
+async def pin(ctx, message: str):
+    message = await ctx.channel.send(message)
+    await message.pin()
+    print("pinned '"+ message.content + "' to #" + ctx.channel.name)
+    await ctx.message.delete()
 
 @teamkills.error
 #@npregister.error
